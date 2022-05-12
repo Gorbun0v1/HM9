d69715fa7756c9560508e13a8cba35aaf2f38bc9
AntonGorbuno <joongleboo01@gmail.com>
2022-05-12 23:08:52 +0300

    Сделал №2:)

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,43 @@ public class Main {
 
     public static void main(String[] args) {
         int year = 2022;
-        ProverkaGoda(year);
+        reviewCode(year);
         int clientOS = 0;
-        ProverkaOS(clientOS);
+        reviewOS(clientOS);
         int deliveryDistance = 95;
-        DostavkaKartiDnei(deliveryDistance);
+        System.out.println("Потребуется дней " + deliverCard(deliveryDistance));
 
 
 
     }
 
-    public static int ProverkaGoda(int year) {
+    private static void reviewCode(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " являеется високосным");
         } else {
             System.out.println(year + " не является високосным");
         }
-        return year;
     }
 
-    public static int ProverkaOS(int clientOS) {
+    private static void reviewOS(int clientOS) {
         int currentYear = LocalDate.now().getYear();
         if (clientOS == 0) {
             if (currentYear >= 2015) {
                 System.out.println("Установите современну версию приложения для iOS по ссылке");
-                return 0;
             } else {
                 System.out.println("Установите lite версию приложения для iOS по ссылке");
-                return 1;
             }
         } else {
             if (clientOS >= currentYear) {
                 System.out.println("Установите современну версию приложения для Android по ссылке");
-                return 0;
             } else {
                 System.out.println("Установите lite версию приложения для Android по ссылке");
-                return 1;
             }
         }
 
     }
 
-    public  static int DostavkaKartiDnei(int deliveryDistance) {
+    private static int deliverCard(int deliveryDistance) {
         int deliveryDays = 1;
         if (deliveryDistance > 20){
             deliveryDays++;
@@ -54,7 +49,6 @@ public class Main {
         if (deliveryDistance > 60){
             deliveryDays++;
         }
-        System.out.println("Потребуется дней " + deliveryDays);
         return deliveryDays;
 
     }
